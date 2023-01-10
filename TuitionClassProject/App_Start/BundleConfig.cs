19f28c20282ec15e62e232f577a22a285ac6228b
Tharindu Lakmal <tharindu96221@gmail.com>
2023-01-10 08:17:32 +0530

    Add DataTable and Bootbox plugins

--- TuitionClassProject/App_Start/BundleConfig.cs
+++ TuitionClassProject/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace TuitionClassProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/bootbox.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +22,13 @@ namespace TuitionClassProject
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/datatables/datatables.bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-paper.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css"));
         }
     }
 }
